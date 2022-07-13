2f1a7b9ec2df26ba05351a7e0fd28604851d4928
waodezalmawati <you@example.com>
2022-07-13 17:15:15 +0800

    coba commit

---
+++ aura.py
@@ -0,0 +1,29 @@
+from matplotlib import container
+import streamlit as st
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import plotly.express as px
+from neo4j import GraphDatabase
+from PIL import Image
+st.set_page_config(layout="wide")
+
+plt.style.use('seaborn')
+
+with open('style.css') as f:
+    st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+
+#Konekdi Database
+drivers = GraphDatabase.driver(uri = "neo4j+s://d4f6a837.databases.neo4j.io", auth = ("neo4j","efnDAFw4yr2ODSk2cqfUBCD0BlsnAtxscFdVOVSx6Xc"))
+session = drivers.session(database='neo4j')
+
+#Query tarik data I
+q1 = """
+match (n:Sublead_CG1) return n.Rain as Rain, n.Holiday as Holiday, n.Customer_Problem as Customer_Problem, n.Slippery as Slippery, n.Produksi as Produksi
+"""
+result1 = session.run(q1)
+data1 = result1.data()
+result_cg1 = pd.DataFrame(data1)
+
+st.write(result_cg1)
